263e85cf423490911649520bf4c1db0daadaddc1
Grant Beaty <gbeaty@gmail.com>
2014-08-10 11:04:16 -0400

    Cleanup.

--- ddaq/src/main/scala/LookupTable.scala
+++ ddaq/src/main/scala/LookupTable.scala
@@ -56,32 +56,4 @@ object LookupTable {
         Some(v1 + diff.times(percent))
       }
   }
-
-  /*def interpolateLinear[K <: MUnit,V <: MUnit](table: Array[(DoubleU[K],DoubleU[V])])(in: DoubleU[K]): Option[DoubleU[V]] =
-    if(table.length < 2 || in < table(0)._1 || in > table(table.length - 1)._1)
-      None
-      else {
-        var i = table.length / 2
-        var step = i
-        var k = 0.0.of[K]
-        var v = 0.0.of[V]
-        while(step > 0) {
-          k = table(i)._1
-          v = table(i)._2
-          if(k == in)
-            return Some(v)
-
-          step = step / 2
-          i = i + (if((in - k) > 0.of[K]) step else -step)
-        }
-        if(k > in) {
-          i -= 1
-          k = table(i)._1
-          v = table(i)._2
-        }
-        val (k2,v2) = table(i+1)
-        val percent = (in - k).value / (k2 - k).value
-        val diff = v2 - v
-        Some(v + diff.times(percent))
-      }*/
 }
